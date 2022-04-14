213447398717f8ec192d82cae293b1c33ea6c7d8
AlekseyPi <tolexis@gmail.com>
2022-04-14 14:19:42 +0200

    Review

--- src/api/companies.controller.ts
+++ src/api/companies.controller.ts
@@ -2,11 +2,16 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { SearchData } from '../common/class/searchData';
 import { BackService } from './back.service';
 
-@Controller('back')
+@Controller('companies')
 export class BackController {
   constructor(private readonly backService: BackService) {}
-  @Post()
+  @Get("search")
   search(@Body() body: SearchData) {
     return this.backService.searchRelevantData(body);
+  }
+
+  @Get()
+  returnWholeList() {
+    return Constants.COMPANIES;
   }
 }
